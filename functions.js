7feba6be205fa5325d6d3b469c3c8a23bf4ea313
Tadiwa Leon Muguta <leonmuguta7@gmail.com>
2022-01-06 15:15:39 +0200

    Added Increase and Decrease functionality, as well as the start button functionality

--- functions.js
+++ functions.js
@@ -1,8 +1,25 @@
-function timer() {
-    let today = new Date();
-    let now = today.getHours( )+ ":" + today.getMinutes() + ":" + today.getSeconds();
+let clicks = 0;
 
-    document.getElementById("time").innerHTML = now;
+function goUpOrDown() {
+    document.getElementById("timer").innerHTML = clicks;
 }
 
-setInterval(timer, 1000);
+function startTimer(){
+    let counter = clicks;
+    let down = setInterval(function() {
+        counter--
+        if (counter >= 0) {
+            timeLeft = document.getElementById("timer");
+            timeLeft.innerHTML = counter;
+        }
+        if (counter === 0) {
+            clearInterval(counter);
+            alert("Time's Up!!!");
+        }
+    }, 1000);
+}
+
+// function resetTimer(){
+//     document.getElementById("timer").innerHTML = 0;
+//     clearInterval();
+// }
